f3acd5fef588d78a2e4a95815b75d9c4bad9fcb8
adrianstando <adrsnek11@live.com>
2020-12-11 12:17:51 +0100

    Controller class to control other classes

---
+++ src/Controller.java
@@ -0,0 +1,103 @@
+import files.*;
+import indexing.*;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class Controller {
+    private final static String poison = "THIS_IS_THE_END.non_existing_extension";
+
+    public static void main(String[] args){
+        String pathZero = "./dysk";
+        int numberOfReadingThreads = 2;
+        int numberOfIndexingThreads = 4;
+
+
+        BlockingQueue<File> files = new ArrayBlockingQueue<>(5);
+        BlockingQueue<FileContent> filesContent = new LinkedBlockingQueue<>();
+
+
+        FileFinder fileFinder = new FileFinder(pathZero, numberOfReadingThreads, files);
+        Thread fileFinderThread = new Thread(fileFinder);
+
+        Thread[] fileReaderThreads = new Thread[numberOfReadingThreads];
+        FileReader[] fileReaders = new FileReader[numberOfReadingThreads];
+        for(int i = 0; i < numberOfReadingThreads; i++){
+            fileReaders[i] = new FileReader(files, filesContent);
+            fileReaderThreads[i] = new Thread(fileReaders[i]);
+        }
+
+        // jesli mamy wiecej indeksujacych
+        if(numberOfIndexingThreads > numberOfReadingThreads){
+            fileReaders[0].PutExtraPoison(numberOfIndexingThreads - numberOfReadingThreads);
+        }
+
+        fileFinderThread.start();
+        for(Thread thread : fileReaderThreads){
+            thread.start();
+        }
+
+        /*try {
+
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+
+        /*for(FileContent content : filesContent){
+            //System.out.println(content.getFile().getPath());
+            //System.out.println(content.getContent());
+            System.out.println(content.getAuthor());
+        }*/
+
+
+
+
+
+
+
+
+
+
+        HashMap<String, Word> map = new HashMap<>();
+        WordsDictionary wordsDictionary = new WordsDictionary(map);
+
+        HashMap<String, Author> map1 = new HashMap<>();
+        AuthorDictionary authorDictionary = new AuthorDictionary(map1);
+
+        Thread[] indexingThreads = new Thread[numberOfIndexingThreads];
+        for(int i = 0; i < numberOfIndexingThreads; i++){
+            indexingThreads[i] = new Thread(new InvertedIndex(filesContent, wordsDictionary, authorDictionary));
+            indexingThreads[i].start();
+        }
+
+        try {
+            for(Thread thread : indexingThreads){
+                thread.join();
+            }
+            fileFinderThread.join();
+            for(Thread thread : fileReaderThreads) {
+                thread.join();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        /*if(numberOfIndexingThreads > numberOfReadingThreads){
+            for(int i = 0; i < numberOfIndexingThreads - numberOfReadingThreads; i++){
+                try {
+                    filesContent.put(new FileContent(new File(poison), "", ""));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }*/
+
+        System.out.println(authorDictionary.toString());
+        System.out.println(wordsDictionary.toString());
+
+    }
+}
