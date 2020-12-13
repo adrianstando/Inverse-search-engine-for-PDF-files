55f76035c4f17eaa69fc6e58463f6dadda4fd228
adrianstando <adrsnek11@live.com>
2020-12-13 14:43:41 +0100

    Add JUnit library and tests

---
+++ src/tests/FileContentTest.java
@@ -0,0 +1,41 @@
+package tests;
+
+import files.FileContent;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileContentTest {
+
+    FileContent fileContent = new FileContent(new File("XXX"), "it is file content", "i am the author");
+    FileContent fileContent2 = new FileContent(new File("/wyklad/wykład1.pdf"), "witam na wykładzie", "i am the author");
+
+    @Test
+    void getFile() {
+        assertEquals(fileContent.getFile(), new File("XXX"));
+        assertNotEquals(fileContent.getFile(), new File("XXX/XXX"));
+
+        assertEquals(fileContent2.getFile(), new File("/wyklad/wykład1.pdf"));
+        assertNotEquals(fileContent2.getFile(), new File("wykład1.pdf"));
+    }
+
+    @Test
+    void getContent() {
+        assertEquals(fileContent.getContent(), "it is file content");
+        assertNotEquals(fileContent.getContent(), "i am the author");
+
+        assertEquals(fileContent2.getContent(), "witam na wykładzie");
+        assertNotEquals(fileContent2.getContent(), "i am the author");
+    }
+
+    @Test
+    void getAuthor() {
+        assertNotEquals(fileContent.getAuthor(), "it is file content");
+        assertEquals(fileContent.getAuthor(), "i am the author");
+
+        assertNotEquals(fileContent2.getAuthor(), "witam na wykładzie");
+        assertEquals(fileContent2.getAuthor(), "i am the author");
+    }
+}
