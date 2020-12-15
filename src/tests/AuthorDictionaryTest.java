c9c34ef62f0bc73640d480a1d2184f0b127ef6de
adrianstando <adrsnek11@live.com>
2020-12-15 15:13:14 +0100

    Add dictionary tests

---
+++ src/tests/AuthorDictionaryTest.java
@@ -0,0 +1,76 @@
+package tests;
+
+import indexing.Author;
+import indexing.AuthorDictionary;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthorDictionaryTest {
+    @Test
+    void authorDictionaryTest(){
+        ConcurrentHashMap<String, Author> dictionary = new ConcurrentHashMap<>();
+        AuthorDictionary authorDictionary = new AuthorDictionary(dictionary);
+
+        List<String> authors1 = Arrays.stream(new String[] {"a", "b", "c", "d"}).collect(Collectors.toList());
+        List<String> authors2 = Arrays.stream(new String[] {"s", "d", "f", "g"}).collect(Collectors.toList());
+
+        List<String> path1 = Arrays.stream(new String[] {"1path1a", "1path2a", "1path3a", "1path4a"}).collect(Collectors.toList());
+        List<String> path2 = Arrays.stream(new String[] {"2path1a", "2path2a", "2path3a", "2path4a"}).collect(Collectors.toList());
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(String a : authors1){
+                    for(String b : path1){
+                        authorDictionary.add(a, b);
+                    }
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(String a : authors2){
+                    for(String b : path2){
+                        authorDictionary.add(a, b);
+                    }
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(new HashSet<>(path1), authorDictionary.getFilesWith("a"));
+        assertEquals(new HashSet<>(path2), authorDictionary.getFilesWith("s"));
+
+        Set<String> check1 = new HashSet<>();
+        check1.addAll(authors1);
+        check1.addAll(authors2);
+        assertEquals(check1, authorDictionary.getAllUniqueKeys());
+
+        HashMap<String, Author> check2 = authorDictionary.getDictionary();
+        assertEquals(check1, check2.keySet());
+
+        Author check3 = check2.get("a");
+        Author check4 = new Author("a");
+        for (String x : path1){
+            check4.add(x);
+        }
+        assertEquals(check4.getFilesConnectedWith(), check3.getFilesConnectedWith());
+
+    }
+}
---
+++ src/tests/WordsDictionaryTest.java
@@ -0,0 +1,93 @@
+package tests;
+
+import indexing.Author;
+import indexing.AuthorDictionary;
+import indexing.Word;
+import indexing.WordsDictionary;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WordsDictionaryTest {
+    @Test
+    void wordDictionaryTest(){
+        ConcurrentHashMap<String, Word> dictionary = new ConcurrentHashMap<>();
+        WordsDictionary wordsDictionary = new WordsDictionary(dictionary);
+
+        List<String> words1 = Arrays.stream(new String[] {"a", "b", "c", "d"}).collect(Collectors.toList());
+        List<String> words2 = Arrays.stream(new String[] {"s", "d", "f", "g"}).collect(Collectors.toList());
+
+        List<String> paths1 = Arrays.stream(new String[] {"1path1a", "1path2a", "1path3a", "1path4a"}).collect(Collectors.toList());
+        List<String> paths2 = Arrays.stream(new String[] {"2path1a", "2path2a", "2path3a", "2path4a"}).collect(Collectors.toList());
+
+        List<Integer> positions1 = Arrays.stream(new Integer[] {5, 6, 7, 23, 45}).collect(Collectors.toList());
+        List<Integer> positions2 = Arrays.stream(new Integer[] {2, 4, 9, 10, 100}).collect(Collectors.toList());
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(String a : words1){
+                    for(String b : paths1){
+                        for(Integer c : positions1) {
+                            wordsDictionary.add(a, b, c);
+                        }
+                    }
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(String a : words2){
+                    for(String b : paths2){
+                        for(Integer c : positions2) {
+                            wordsDictionary.add(a, b, c);
+                        }
+                    }
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(new HashSet<>(paths1), wordsDictionary.getFilesWith("a"));
+        assertEquals(new HashSet<>(paths2), wordsDictionary.getFilesWith("s"));
+
+        assertEquals(new HashSet<>(positions1) , new HashSet<>(wordsDictionary.getPositionsOfWordInPath("a", "1path1a")));
+        assertEquals(new HashSet<>(positions1) , new HashSet<>(wordsDictionary.getPositionsOfWordInPath("a", "1path4a")));
+        assertEquals(new HashSet<>(positions2) , new HashSet<>(wordsDictionary.getPositionsOfWordInPath("f", "2path4a")));
+        assertEquals(new HashSet<>(positions2) , new HashSet<>(wordsDictionary.getPositionsOfWordInPath("g", "2path4a")));
+
+        Set<String> check1 = new HashSet<>();
+        check1.addAll(words1);
+        check1.addAll(words2);
+        assertEquals(check1, wordsDictionary.getAllUniqueKeys());
+
+        HashMap<String, Word> check2 = wordsDictionary.getDictionary();
+        assertEquals(check1, check2.keySet());
+
+        Word check3 = check2.get("a");
+        Word check4 = new Word("a");
+        for (String x : paths1){
+            for (Integer y : positions1) {
+                check4.add(x, y);
+            }
+        }
+        assertEquals(check4.getFilesConnectedWith(), check3.getFilesConnectedWith());
+
+    }
+
+}
