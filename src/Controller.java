5e3bea14aa8fee8e18beff63506dd98494b5b407
adrianstando <adrsnek11@live.com>
2020-12-18 17:39:28 +0100

    Add code which will enable serializing dictionaries.

--- src/Controller.java
+++ src/Controller.java
@@ -1,11 +1,15 @@
 import files.*;
+import files.FileReader;
 import indexing.*;
 
-import java.io.File;
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.stream.Collectors;
 
 /**
  * Class which will be something like API for the Inverted Index program.
@@ -14,6 +18,15 @@ import java.util.concurrent.LinkedBlockingQueue;
 // pamietaj tutaj o nullach
 public class Controller {
     private final static String poison = "THIS_IS_THE_END.non_existing_extension";
+    private WordsDictionary wordsDictionary;
+    private AuthorDictionary authorDictionary;
+
+    private static List<String> commonWords = Arrays.stream(new String [] {"a", "able", "about", "all", "an", "and", "any", "are", "as", "at", "be", "been", "by",
+            "can", "can't", "could", "couldn't", "do", "does", "doesn't", "don't", "down", "has", "hasn't", "have", "haven't", "he", "here", "his", "how",
+            "I", "I'm", "if", "in", "is", "it", "its", "it's", "just", "like", "many", "much", "no", "not", "now", "of", "on", "one",
+            "or", "she", "so", "than", "that", "the", "them", "then", "there", "these", "they", "this", "those", "to", "too", "up", "very", "was", "we", "were",
+            "what", "when", "where", "which", "who", "will", "won't", "would", "you", "you'd", "you'll"}).map(String::toLowerCase).map(r -> r.replaceAll("\\p{Punct} | ’", "")).collect(Collectors.toList());
+
 
     public static void main(String[] args){
         //String pathZero = "./dysk0";
@@ -76,7 +89,7 @@ public class Controller {
 
         Thread[] indexingThreads = new Thread[numberOfIndexingThreads];
         for(int i = 0; i < numberOfIndexingThreads; i++){
-            indexingThreads[i] = new Thread(new InvertedIndex(filesContent, wordsDictionary, authorDictionary));
+            indexingThreads[i] = new Thread(new InvertedIndex(filesContent, wordsDictionary, authorDictionary, commonWords));
             indexingThreads[i].start();
         }
 
@@ -113,5 +126,80 @@ public class Controller {
         // i tam gdzie nowa linia, dodajmy spację, by rozdzielic slowa
         out = out.replaceAll("\n", " \n");
         //System.out.println("’".matches("(\n | \\p{Blank} | \\p{IsAlphabetic} | \\p{Digit} | \\p{Punct})")); */
+    }
+
+
+
+
+
+
+    public boolean writeDictionariesToFile(String path){
+        // Create SerializableDictionaries object
+        SerializableDictionaries object = new SerializableDictionaries();
+        object.setAuthorDictionary(authorDictionary);
+        object.setWordsDictionary(wordsDictionary);
+
+        //Saving object to file
+        FileOutputStream file = null;
+        try {
+            file = new FileOutputStream(path);
+            ObjectOutputStream out = new ObjectOutputStream(file);
+            out.writeObject(object);
+
+            out.close();
+            file.close();
+
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    public boolean readDictionariesFromFile(String path){
+        // Create Stream
+        FileInputStream file = null;
+        try {
+            file = new FileInputStream(path);
+            ObjectInputStream in = new ObjectInputStream(file);
+
+            // Create SerializableDictionaries object
+            SerializableDictionaries object = null;
+            object = (SerializableDictionaries)in.readObject();
+
+            in.close();
+            file.close();
+
+            // Save data to this class attributes
+            authorDictionary = object.getAuthorDictionary();
+            wordsDictionary = object.getWordsDictionary();
+
+            return true;
+        } catch (IOException | ClassNotFoundException e) {
+            return false;
+        }
+    }
+
+    private static class SerializableDictionaries implements Serializable {
+        private AuthorDictionary authorDictionary;
+        private WordsDictionary wordsDictionary;
+
+        public SerializableDictionaries() {
+        }
+
+        public AuthorDictionary getAuthorDictionary() {
+            return authorDictionary;
+        }
+
+        public void setAuthorDictionary(AuthorDictionary authorDictionary) {
+            this.authorDictionary = authorDictionary;
+        }
+
+        public WordsDictionary getWordsDictionary() {
+            return wordsDictionary;
+        }
+
+        public void setWordsDictionary(WordsDictionary wordsDictionary) {
+            this.wordsDictionary = wordsDictionary;
+        }
     }
 }
