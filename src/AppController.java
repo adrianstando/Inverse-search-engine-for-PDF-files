63519f4be789eab6f5fa953425222a48eb7a55ab
adrianstando <adrsnek11@live.com>
2020-12-30 12:42:46 +0100

    Add waiting alerts

--- src/AppController.java
+++ src/AppController.java
@@ -1,5 +1,6 @@
 import controller.Controller;
 import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
@@ -8,6 +9,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
@@ -16,6 +18,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BooleanSupplier;
 
 public class AppController {
     public Button buttonChoosePathToIndex;
@@ -40,8 +43,14 @@ public class AppController {
         DirectoryChooser directoryChooser = new DirectoryChooser();
         File selectedDirectory = directoryChooser.showDialog(null);
         if(!(selectedDirectory == null)){
-            controller.createIndex(selectedDirectory.getAbsolutePath());
-            indexLoaded = true;
+            generateWaitingAlertAndDoTask("Building Index",
+                    "The index is being built from path: \n" + selectedDirectory.getAbsolutePath(),
+                    "Error!",
+                    () -> {
+                        controller.createIndex(selectedDirectory.getAbsolutePath());
+                        indexLoaded = true;
+                        return true;
+                    });
         }
     }
 
@@ -54,11 +63,21 @@ public class AppController {
         FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("SER files (*.ser)", "*.ser");
         fileChooser.getExtensionFilters().add(extFilter);
         File selectedFile = fileChooser.showOpenDialog(null);
-        if(!(selectedFile == null)){
-            boolean b = controller.readDictionariesFromFile(selectedFile.getAbsolutePath());
 
-            if(!b) generateErrorAlert("Error during reading index file!");
-            else indexLoaded = true;
+        if(!(selectedFile == null)){
+            generateWaitingAlertAndDoTask("Reading Index",
+                    "The index is being read from path: \n" + selectedFile.getAbsolutePath(),
+                    "Error during reading index from file!",
+                    () -> {
+                        boolean b = controller.readDictionariesFromFile(selectedFile.getAbsolutePath());
+
+                        if (!b){
+                            return false;
+                        } else {
+                            indexLoaded = true;
+                            return true;
+                        }
+                    });
         }
     }
 
@@ -73,10 +92,19 @@ public class AppController {
         File file = fileChooser.showSaveDialog(null);
 
         if(!(file == null)){
-            boolean b = controller.writeDictionariesToFile(file.getAbsolutePath());
+            generateWaitingAlertAndDoTask("Reading Index",
+                    "The index is being written to path: \n" + file.getAbsolutePath(),
+                    "Error during writing index to file!",
+                    () -> {
+                        boolean b = controller.writeDictionariesToFile(file.getAbsolutePath());
 
-            if(!b) generateErrorAlert("Error during writing index to file!");
-            else indexLoaded = true;
+                        if (!b){
+                            return false;
+                        } else {
+                            indexLoaded = true;
+                            return true;
+                        }
+                    });
         }
     }
 
@@ -167,6 +195,7 @@ public class AppController {
         alert.setTitle("Error");
         alert.setHeaderText(text);
 
+        // bug in KDE Linux - this must be added
         // this makes alerts work on KDE Linux (Kubuntu)
         alert.setResizable(true);
         alert.onShownProperty().addListener(e -> {
@@ -174,6 +203,55 @@ public class AppController {
         });
 
         alert.showAndWait();
+    }
+
+    /**
+     * Method which generates waiting alert with given title and text. Alert hides after function func ends.
+     * @param title
+     * @param text
+     * @param func
+     */
+    private void generateWaitingAlertAndDoTask(String title, String text, String textOnError, BooleanSupplier func){
+        Alert alert = new Alert(Alert.AlertType.NONE);
+        alert.setTitle(title);
+
+        Text textAlert = new Text(text);
+        textAlert.setWrappingWidth(500);
+        alert.getDialogPane().setContent(textAlert);
+
+        ProgressIndicator progressIndicator = new ProgressIndicator();
+        alert.setGraphic(progressIndicator);
+
+        // bug in KDE Linux - this must be added
+        // this makes alerts work on KDE Linux (Kubuntu)
+        alert.setResizable(true);
+        alert.onShownProperty().addListener(e -> {
+            Platform.runLater(() -> alert.setResizable(false));
+        });
+
+        Task<Boolean> task = new Task<Boolean>() {
+            @Override
+            protected Boolean call() throws Exception {
+                return func.getAsBoolean();
+            }
+        };
+
+        // when task ends, the alert disappears
+        task.setOnSucceeded(e ->{
+            alert.getDialogPane().getButtonTypes().addAll(ButtonType.CANCEL);
+            alert.close();
+        } );
+        task.setOnFailed(e -> {
+            alert.getDialogPane().getButtonTypes().addAll(ButtonType.CANCEL);
+            alert.close();
+            generateErrorAlert(textOnError);
+        });
+
+
+        // run alert and task
+        alert.show();
+        Thread taskThread = new Thread(task);
+        taskThread.start();
     }
 
 }
